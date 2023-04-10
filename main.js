9ff08b0b3825aa8fafd6d2b5c5e65b8ac0b399b8
Danayet Gezaie <124500874+Danayet@users.noreply.github.com>
2023-04-10 15:59:50 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-/* 
-👉 Write your kata here!
+/* The traffic light is red and has a stop sign, have the traffic light change to yellow and return slow down, and then back to green to return go. To simulate the behavior of a traffic light that changes from red to yellow and then to green, you can use a combination of setTimeout() and Promise
+
+Tips
+You can use setTimeout() to imitate the traffic light sequence.
+The function should return a string.
+The function should have a delay of 2 seconds between each light change.
+You can assume that the function is called in the correct sequence (red, yellow, green).
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+function trafficLight() {
+  // TODO: implement the traffic light logic here
+}
