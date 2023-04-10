557e3c48a5164a54fab736f2a031684d9d643adf
Danayet Gezaie <124500874+Danayet@users.noreply.github.com>
2023-04-10 21:55:58 +0100

    Update main.test.js

--- main.test.js
+++ main.test.js
@@ -1,1 +1,20 @@
 //👉 Write your tests below here:
+export function trafficLight() {
+  return new Promise((resolve) => {
+    console.log("Stop"); // red light with stop sign
+    setTimeout(() => {
+      console.log("Slow down"); // yellow light
+      setTimeout(() => {
+        console.log("Go"); // green light
+        resolve();
+      }, 2000); // wait for 2 seconds before changing to green
+    }, 2000); // wait for 2 seconds before changing to yellow
+  });
+}
+
+import { trafficLight } from './trafficLight.js';
+
+// Call the trafficLight function
+trafficLight().then(() => {
+  console.log('Finished');
+});
